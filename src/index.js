6240621e4b3f9f61ed1325d75220abc265073fc7
guangyujia <15210735793@163.com>
2022-12-02 07:19:33 +0800

    对js,css里的引用Incon进行解析转化

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import _ from 'lodash';
 import './index.css';
+import Icon from './chinese_icon.png';
 
 (function component() {
     const app = document.getElementById('app');
     const element = document.createElement('div');
     element.classList.add('box');
     element.innerHTML = _.join(['Hello', 'webpack3'], ['']);
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    element.appendChild(myIcon);
     app.appendChild(element);
 })()
