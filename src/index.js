45cf47a1d81dd9b44398db9e04b339da49d48fd0
guangyujia <15210735793@163.com>
2022-12-01 19:28:49 +0800

    添加 webpack.config.js, 手动执行 npx webpack --config  webpack.config.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import _ from 'lodash';
 
 function component() {
     const element = document.getElementById('app')
-    element.innerHTML = _.join(['Hello', 'webpack'], ['']);
+    element.innerHTML = _.join(['Hello', 'webpack2'], ['']);
     return element;
 }
 
