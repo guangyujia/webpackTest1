bc48baaf9c9c669a2eb33fdca3622f9bf2c38885
guangyujia <15210735793@163.com>
2022-12-02 11:25:13 +0800

    对于下篇指南，我们无需使用本指南中所有用到的资源，因此我们会进行一些清理工作，以便为下篇指南 管理输出 做好准备

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import _ from 'lodash';
-import './index.css';
-import Icon from './chinese_icon.png';
-import json from './data.json5';
 
-(function component() {
-    const app = document.getElementById('app');
+const app = document.getElementById('app');
+function component() { 
     const element = document.createElement('div');
-    element.classList.add('box');
     element.innerHTML = _.join(['世界', '你好'], ['']);
-    const myIcon = new Image();
-    myIcon.src = Icon;
-    
-    console.log(json.title)
-    console.log(json.owner.name)
-    element.appendChild(myIcon);
-    app.appendChild(element);
-})()
+    return element; 
+}
+app.appendChild(component());
