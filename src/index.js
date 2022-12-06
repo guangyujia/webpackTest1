399d0ad321dcfac5dbb4550e5d7993cf37dbb81e
guangyujia <15210735793@163.com>
2022-12-06 17:37:58 +0800

    我们可以看到这三个文件的 hash 都变化了。这是因为每个 module.id 会默认地基于解析顺序(resolve order)进行增量。也就是说，当解析顺序发生变化，ID 也会随之改变。简要概括：
    main bundle 会随着自身的新增内容的修改，而发生变化。
    vendor bundle 会随着自身的 module.id 的变化，而发生变化。
    manifest runtime 会因为现在包含一个新模块的引用，而发生变化

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import _ from 'lodash';
+import Print from './print';
+
 function getComponent() { 
     return import('lodash')
     .then(({default: _}) => {
         const element = document.createElement('div');
         element.innerHTML = _.join(['世界', '你好'], ['']);
+        element.onclick = Print.bind(null, 'Hello webpack!')
         return element;
     })
     .catch(err => 'An error occured while loading the component');
