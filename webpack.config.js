eb16be0860d44c8a613e939bab690547517eee7a
guangyujia <15210735793@163.com>
2022-12-06 08:24:21 +0800

    防止重复 配置 dependOn option 选项，这样可以在多个 chunk 之间共享模块

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,23 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: {
-        index: './src/index.js',
-        another: './src/another-module.js'
+        index: {
+            import: './src/index.js',
+            dependOn: 'shared'
+        },
+        another: {
+            import: './src/another-module.js',
+            dependOn: 'shared'
+        },
+        shared: 'lodash'
     },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true
+    },
+    optimization: {
+        runtimeChunk: 'single'
     },
     devtool: 'inline-source-map',
     plugins: [
